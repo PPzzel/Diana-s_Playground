d8bfaafb4737737737fd4a3c67d8a034b5f23785
PPzzel <45909987+PPzzel@users.noreply.github.com>
2018-12-16 17:01:28 -0500

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,15 @@
+const Discord = require("discord.js");
+const client = new Discord.Client;
+
+client.on('ready', () => {
+console.log('I am ready!');
+});
+
+client.on("message", message => {
+  if(message.content == "ping"){
+    message.reply("Pong");
+  }
+  });
+  
+  client.login("NTIzNzM3NzgyMDExNTU5OTM2.Dvd5QQ.3ZUGx77vXipMmdbYgJ-SYOG32gA");
+  
