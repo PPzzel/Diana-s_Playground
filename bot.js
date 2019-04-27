5c8ad3b9504e08624006850d873deee9ab9b5c75
PPzzel <45909987+PPzzel@users.noreply.github.com>
2019-04-27 13:55:38 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,19 +11,11 @@ client.on("message", message => {
     
   }
   });
-  
-  client.login(process.env.B0T_T0KEN);
-
-const Discord = require("discord.js");
-const client = new Discord.Client;
-
-client.on('ready', () => {
-console.log('I am ready!');
 });
 
 client.on("message", message => {
-  if(message.content == "Ding"){
-    message.reply("Dong");
+  if(message.content == "pang"){
+    message.reply("Pong");
     
   }
   });
