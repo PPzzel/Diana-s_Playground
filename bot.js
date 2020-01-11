b07d12e8b8026588e48dfefabf2f5225de7fb4b3
PPzzel <45909987+PPzzel@users.noreply.github.com>
2020-01-11 17:13:21 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,3 @@
-    
 const Discord = require("discord.js");
 const client = new Discord.Client;
 
@@ -7,10 +6,10 @@ console.log('I am ready!');
 });
 
 client.on("message", message => {
-  if(message.content == "Bing"){
-    message.reply("Pong"); 
+  if(message.content == "ping"){
+    message.reply("Pong");
     
-  } 
+  }
   });
   
   client.login(process.env.B0T_T0KEN);
